d0615f370f7b8f7ba5aa13cd3d22d3f5b351a92e
osmantrend <osmancibik@icloud.com>
2022-02-10 16:16:23 +0000

    console print

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const app =  require( "./app");
 const port = process.env.PORT || 3001;
 
+console.log(`Platform : ${os.platform()}`);
+console.log(`Free Memory : ${os.freemem()/(1024*1024*1024)}`)
+console.log(`Total Memory : ${os.totalmem()/(1024*1024*1024)}`)
+
 app.listen(port, () =>
   console.log('Example app listening on port 3000!'),
 );
